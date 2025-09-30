3770f9c7ba1af0d34f0e0cabde9990d422bf2a4b
mraibel <mraibel12@gmail.com>
2025-09-30 09:45:10 -0300

    Estructura servidor

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Importar los modelos
+/*
+Ejemplo:
+const User = require('./model/user.model.js')
+*/
+
+// Importar las rutas
+/*
+Ejemplo: 
+const usuariosRoutes = require('./routes/usuarioRoutes.js')
+*/
+
+
+// Asociaciones entre modelos
+/* 
+Ejemplo: 
+User.belongsToMany(Rol, { as:'rolUser', through: UserRol, foreignKey:'id_usuario' })
+Rol.belongsToMany(User, { as:'userRol', through: UserRol, foreignKey:'id_rol' })
+*/
+
+
+// Fin Asociaciones ============================================================================================================
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Exponer las rutas
+// Ejemplo
+// app.use('/api/usuarios', usuariosRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('Ruta no encontrada')
+})
+
+module.exports = app
